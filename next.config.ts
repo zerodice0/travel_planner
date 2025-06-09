4a18cd33bdff68dd0ae2230b6d63b726099c2bab
zerodice0 <zerodice0@gmail.com>
2025-06-10 08:51:07 +0900

    build: Turbopack과 충돌하는 소스맵 형식 문제 해결을 위해 evl-source-map 적용.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev }) => {
+    if (dev) {
+      // Turbopack과 충돌하는 소스맵 형식 문제 해결을 위한 eval-source-map 설정
+      config.devtool = 'eval-source-map';
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
