3b9108278b635deb4119de122c2369b0ded3c367
zerodice0 <zerodice0@gmail.com>
2025-06-10 09:06:20 +0900

    build: webpack 관련 옵션 제거 및 eval-source-map 옵션 제거.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      // Turbopack과 충돌하는 소스맵 형식 문제 해결을 위한 eval-source-map 설정
-      config.devtool = 'eval-source-map';
-    }
-    return config;
-  }
+  /* config options here */
 };
 
 export default nextConfig;
