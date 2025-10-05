d7362adf65ffc3c1db8aacab08a089c0d90a7c0b
zerodice0 <zerodice0@gmail.com>
2025-10-05 09:42:19 +0900

    프로젝트 초기화

--- migrations/004_add_initial_location_to_trips.sql
+++
@@ -1,8 +0,0 @@
--- Add initial location fields to trips table
-ALTER TABLE trips 
-ADD COLUMN initial_latitude DOUBLE PRECISION,
-ADD COLUMN initial_longitude DOUBLE PRECISION;
-
--- Add comment for documentation
-COMMENT ON COLUMN trips.initial_latitude IS 'Initial map center latitude for trip';
-COMMENT ON COLUMN trips.initial_longitude IS 'Initial map center longitude for trip';
--- src/widgets/trip-place-list/index.ts
+++
@@ -1,1 +0,0 @@
-export { TripPlaceList } from './TripPlaceList'; 
